cd17448cc6e58e47c635ee48f82de2a98cf1015c
Giorgio Ruiton <gpaoloruitonm@gmail.com>
2022-07-07 12:13:44 -0500

    Fix

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import "./_App.scss";
 import React from "react";
 import Header from "./Component/Header/Header";
 import Main from "./Component/main/Main";
-import { BrowserRouter as Router } from "react-router-dom";
 import projectsList from "./data/list_projects";
 import Footer from "./Component/Footer";
 // import NavBar from "./Component/Header/NavBar";
 
 function App() {
   return (
-    <Router>
+    <>
       <Header />
       <Main projectsList={projectsList} />
       <Footer />
-    </Router>
+    </>
   );
 }
 
